694be51d6553df1b2dd02fed3229aa1d07bfb3fb
Robert Vannest <121450248+Rvannest@users.noreply.github.com>
2024-04-09 13:50:27 -0400

    still stuck on blockchain transaction failed, trying to debug with tenderly

--- pancakeswaptest.py
+++ pancakeswaptest.py
@@ -34,13 +34,14 @@ account_privatekey = w3.eth.account.from_key(Private_Key)
 
 # Pancakeswap swap router, swap router abi is at the top of the code
 swap_router_address = "0x9a489505a00cE272eAa5e07Dba6491314CaE3796" # swap router v3, bsc testnet (near the bottom of the address documentation)
+# swap_router_address = "0x1b81D678ffb9C0263b24A97847620C99d213eB14" #swap router v3, periphery bsc testnet
 swap_router_contract = w3.eth.contract(address=swap_router_address, abi=swap_router_abi)
 
 wbnb_contract_address = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"
 wbnb_contract = w3.eth.contract(address=wbnb_contract_address, abi=wbnb_testnet_abi)
 
-# 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c WBNB on mainnet, 0x55d398326f99059fF775485246999027B3197955 USDT on Mainnet
-# 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd WBNB on testnet, 0x337610d27c682E347C9cD60BD4b3b107C9d34dDd USDT on testnet
+# 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c WBNB on mainnet, 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd WBNB on testnet,
+# 0x55d398326f99059fF775485246999027B3197955 USDT on Mainnet, 0x337610d27c682E347C9cD60BD4b3b107C9d34dDd USDT on testnet
 
 # Approve the WBNB spend
 amount_in = w3.to_wei(0.05, "ether")
@@ -60,9 +61,9 @@ w3.eth.wait_for_transaction_receipt(approve_txn_hash)
 
 # parameters for the swap
 params = {
-  "tokenIn": Web3.to_checksum_address("0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"), # now its WBNB
-  "tokenOut": Web3.to_checksum_address("0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"), #USDT
-  "fee": 3000, # fee tier of the pool 0.3%
+  "tokenIn": Web3.to_checksum_address("0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd"), # now its WBNB testnet
+  "tokenOut": Web3.to_checksum_address("0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"), #USDT testnet
+  "fee": 500, # fee tier of the pool 0.3%
   "recipient": Web3.to_checksum_address("0x638c1546faE0Ce97E1524563F9AE0c42127DbBeE"), #test wallet address
   "deadline": int(time.time()) + 600, #10 mins from now
   "amountIn": w3.to_wei(0.01, "ether"), #amount of wbnb to swap
